afab36165e87662edda2428fb16d5032d77d438c
Dhiyaul-n <140733237+Dhiyaul-n@users.noreply.github.com>
2023-10-31 11:39:02 +0700

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,79 @@
+// let i = 10 ;
+// console.log(i)
+
+// for ( var i = 0 ; i < 10 ; i++ ) {
+//     console.log(i)
+// }
+
+// function tes() {
+//     for ( var i = 0; i < 10; i++){
+//       console.log(i)
+//     }
+// }
+// tes();
+
+// (function(){
+//   for( var i = 0; i < 10; i ++ ) {
+//     console.log(i)
+//   }
+// }());
+
+// for ( let i = 0 ; i < 10; i++ ) {
+//     console.log(i);
+// }
+
+// const mhs = {
+//   nama : "dhiyaul",
+//   umur : 17
+// }
+
+// mhs = {
+//   jurusan : "teknik informatika"
+// }
+
+// console.log(mhs)
+
+
+// function expression
+
+
+// const tampilnama = function (nama) {
+//   return `Halo, ${nama}`;
+// }
+// console.log(tampilnama('dhiyaul'))
+
+
+// arrow function
+
+// const tampilama = (nama) => {return `halo, ${nama}`; }
+// console.log('dhiyaul');
+
+
+//
+
+
+// const tampilnama = (waktu, nama) => {
+//   return `selamat ${waktu}, ${nama}`;
+// }
+// console.log(tampilnama('siang','dhiyaul'))
+
+// implisit return
+
+// const tampilnama = nama => `halo,${nama}`;
+// console.log('dhiyaul nawaz .J');
+
+
+
+// sebelum arow function
+
+// let mahasiswa = ['dhiyaul nawaz','dhiyaul','nawaz'];
+
+// let jumlahhuruf = mahasiswa.map(function(nama)  {
+//   return nama.length;
+// });
+// console.log(jumlahhuruf)
+
+// let mahasiswa = ['dhiyaul','nawaz','janan'];
+
+// let jumlahhuruf = mahasiswa.map(nama=> nama.length);
+// console.log(jumlahhuruf);
